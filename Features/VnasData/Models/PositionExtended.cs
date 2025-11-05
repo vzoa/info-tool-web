96e7b713e4159e9d0ef9964028fea4b4f932f16d
Ken Greim <34892440+kengreim@users.noreply.github.com>
2025-11-05 13:06:31 -0800

    Display TCPs properly on positions page

--- Features/VnasData/Models/PositionExtended.cs
+++ Features/VnasData/Models/PositionExtended.cs
@@ -1,11 +1,11 @@
 ﻿namespace ZoaReference.Features.VnasData.Models;
 
-public class PositionExtended(Position basePosition, string tcp)
+public class PositionExtended(Position basePosition, string? tcp)
 {
     public string Name => basePosition.Name ?? "";
     public string Callsign => basePosition.Callsign ?? "";
     public string RadioName => basePosition.RadioName ?? "";
     public int Frequency => basePosition.Frequency;
 
-    public string Tcp => tcp;
+    public string Tcp => tcp ?? "";
 }
